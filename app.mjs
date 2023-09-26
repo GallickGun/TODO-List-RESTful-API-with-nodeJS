d81a799d67a84959248f40c42f529b155da1a0c2
GallickGun <gallickgun@GallickGuns-MacBook-Pro.local>
2023-09-26 14:59:55 +0700

    update todo

--- app.mjs
+++ app.mjs
@@ -6,10 +6,12 @@ const server = http.createServer((request, response) => {
 
     response.setHeader("Content-Type", "application/json");
 
-    if(request.method === "GET"){
+    if (request.method === "GET"){
         service.getTodoList(request, response)
-    }else if(request.method === "POST"){
+    }else if (request.method === "POST"){
         service.createTodo(request, response)
+    }else if (request.method === "PUT"){
+        service.updateTodo(request, response)
     }
 })
 
